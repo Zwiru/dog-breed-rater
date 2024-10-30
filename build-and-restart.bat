452d0816d4ced6b2cc9ca78d0ed7d08287b867a4
Piotr <ptrkasiewicz@gmail.com>
2024-10-30 22:13:01 +0100

    fix in build-and-restart.bat

--- build-and-restart.bat
+++ build-and-restart.bat
@@ -1,4 +1,4 @@
 docker build . -t dog-breed-rater
-docker stop dog-breed-rater
-docker rm dog-breed-rater
+docker stop dog-breed-rater || true
+docker rm dog-breed-rater || true
 docker run -d -p 8080:8080 --name=dog-breed-rater dog-breed-rater

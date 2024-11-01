5b0a2f83626865e01fcfd66927e95a58b3bd1ae9
Piotr <ptrkasiewicz@gmail.com>
2024-11-01 23:13:14 +0100

    update build-and-restart.bat

--- build-and-restart.bat
+++ build-and-restart.bat
@@ -1,6 +1,4 @@
 docker build . -t dog-breed-rater
 docker stop dog-breed-rater || true
 docker rm dog-breed-rater || true
-docker run -d -p 8080:8080 --name=ogloszex --network dbr-network --restart unless-stopped dog-breed-rater
-
-@REM docker run -d -p 8080:8080 --name=ogloszex -e SPRING_PROFILES_ACTIVE=prod --network dbr-network --restart unless-stopped dog-breed-rater
+docker run -d -p 8080:8080 --name=dog-breed-rater --network dbr-network --restart unless-stopped dog-breed-rater

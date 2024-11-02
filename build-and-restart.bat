3b86395dbfc561dc22a299b728be0da87174c0a1
Piotr <ptrkasiewicz@gmail.com>
2024-11-03 00:17:27 +0100

    fix build-and-restart.bat

--- build-and-restart.bat
+++ build-and-restart.bat
@@ -1,8 +1,4 @@
 docker build . -t dog-breed-rater
 docker stop dog-breed-rater || true
 docker rm dog-breed-rater || true
-<<<<<<< Updated upstream
 docker run -d -p 8080:8080 --name=dog-breed-rater --network dbr-network --restart unless-stopped dog-breed-rater
-=======
-docker run -d -p 8080:8080 --name=dog-breed-rater --network dbr-network --restart unless-stopped dog-breed-rater
->>>>>>> Stashed changes
